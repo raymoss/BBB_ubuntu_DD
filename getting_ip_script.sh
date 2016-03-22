f91a1970abe3c57f14603b43cc5bbbb87de78399
Ray Moss <itsraymoss@gmail.com>
2016-03-22 12:26:37 +0530

    editted the interface getting ip script

--- getting_ip_script.sh
+++ getting_ip_script.sh
@@ -7,7 +7,7 @@ touch ip_log.txt
 echo "Subject: IP address" >>ip_log.txt
 echo "getting the ip" >> ip_log.txt
 echo "My current ip :" >> ip_log.txt 
-ifconfig wlp15s0 | grep "inet addr" | cut -d: -f2 | awk '{print $1}' >> ip_log.txt 2>&1
+ifconfig eth0 | grep "inet addr" | cut -d: -f2 | awk '{print $1}' >> ip_log.txt 2>&1
 echo "My public ip :" >>ip_log.txt
 wget http://ipinfo.io/ip -qO - >> ip_log.txt
 echo "Sending mail" >>ip_log.txt
